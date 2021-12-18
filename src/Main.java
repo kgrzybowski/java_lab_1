8547511e6c2b83b96cc02f6db2f4e85de8da2af5
Karol Grzybowski <k.grzybowski11@gmail.com>
2021-12-18 12:50:06 +0100

    start

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,36 @@ public class Main {
         *   wynik wyświetlić w konsoli programu
         */
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Podaj imię: ");
+        String imie = scan.next();
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scan.next();
+        System.out.print("Wiek: ");
+        int wiek = scan.nextInt();
+        System.out.print("Nr albumu: ");
+        int nrAlbumu = scan.nextInt();
+
+        System.out.println("Twoje imię i nazwisko to: "+ imie + " " + nazwisko + " "+ ", a wiek : " + wiek + " " + "nr albumu : " + nrAlbumu);
+        System.out.printf("Twoje imię i nazwisko to: %s %s a wiek i numer indeksu %d %d \n", imie, nazwisko, wiek, nrAlbumu);
+
+        int liczbaA = 10, liczbaB = 2;
+        int dodawanie = liczbaA + liczbaB;
+        int odejmowanie = liczbaA - liczbaB;
+        int mnozenie = liczbaA * liczbaB;
+        int dzielenie = liczbaA / liczbaB;
+        int modulo = liczbaA % liczbaB;
+
+        System.out.println("dodawanie liczby A i B wynosi " + dodawanie);
+        System.out.println("odejmowanie liczby A i B wynosi " + odejmowanie);
+        System.out.println("mnożenie liczby A i B wynosi " + mnozenie);
+        System.out.println("dzielenie liczby A i B wynosi " + dzielenie);
+        System.out.println("reszta z liczby A i B wynosi " + modulo);
+
+
+
+
 
     }
 }
