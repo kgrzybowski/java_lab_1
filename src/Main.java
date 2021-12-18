0b521f3c9253bbfe12973cec4ddca3d1ba4fe4ee
Karol Grzybowski <k.grzybowski11@gmail.com>
2021-12-18 11:20:32 +0100

    start

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("Cześć");
+        System.out.println("Tu Karol");
+        System.out.println("Uczę" + " " + "się" + " " + "programować!");
 
+        System.out.printf("A %s zaczynamy z %d przykładem.\n", "więc", 1);
+
+        System.out.print("To jest jakiś");
+        System.out.print(" tekst");
+        System.out.print(" nie za długi.");
     }
 }
