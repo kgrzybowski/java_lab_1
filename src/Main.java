d18208cc01fe0e3c8fa71fce3a8093b68b415c83
Karol Grzybowski <k.grzybowski11@gmail.com>
2021-12-18 12:12:39 +0100

    start

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int liczbaA, liczbaB, liczbaC, liczbaD = 5, liczbaE = 20, liczbaF = 30;
+                liczbaA = 10;
+                liczbaB = 2;
+                liczbaC = 4;
+        short liczbaG = 100;
+        byte liczbaH = 15;
+        double liczbaX = 10.5, liczbaY = 2.75;
 
-        int zmiennaInt = 7;
-        System.out.println("zmienna int = " + zmiennaInt);
+        int dodawanie = liczbaA + liczbaB;
+        int odejmowanie = liczbaE - liczbaA;
+        int mnozenie = liczbaC * liczbaD;
+        int dzielenie = liczbaE / liczbaC;
+
+        double dodawanieXY = liczbaX + liczbaY;
+        double odejmowanieXY = liczbaX - liczbaY;
+        double mnozenieXY = liczbaX * liczbaY;
+        double dzielenieXY = liczbaX / liczbaY;
+
+        System.out.println("dodawanie liczby A i B wynosi " + dodawanie);
+        System.out.println("odejmowanie liczby E i A wynosi " + odejmowanie);
+        System.out.println("mnożenie liczby C i D wynosi " + mnozenie);
+        System.out.println("dzielenie liczby E i C wynosi " + dzielenie);
+        System.out.println("");
+        System.out.println("dodawanie liczby X i Y wynosi " + dodawanieXY);
+        System.out.println("odejmowanie liczby X i Y wynosi " + odejmowanieXY);
+        System.out.println("mnożenie liczby X i Y wynosi " + mnozenieXY);
+        System.out.println("dzielenie liczby X i Y wynosi " + dzielenieXY);
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
